ab15e159f77460d3d16261bc34d1cc6bfef618ba
Sam Backus <sbackus@kayak.com>
2015-04-07 14:29:29 -0400

    draw the ball

--- pong.rb
+++ pong.rb
@@ -1,22 +1,34 @@
-require "Hasu"
+require 'rubygems'
+require 'Hasu'
 
 Hasu.load "ball.rb"
 
 class Pong < Hasu::Window
+  WIDTH = 640
+  HEIGHT = 480
+
   def initialize
-    super(640, 480, false)
+    super(WIDTH, HEIGHT, false)
   end
 
   def reset
-    # ...
+    @ball = Ball.new
   end
 
   def update
-    # ...
+    
   end
 
   def draw
-    # ...
+    @ball.draw(self)
+  end
+
+  def button_down(id)
+
+  end
+
+  def button_up(id)
+
   end
 end
 
